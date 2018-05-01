c682b107d4a5742bb4ead5f45757ffc651f26db1
AustinPaley <austinpaley32@gmail.com>
2018-05-01 10:07:52 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -14,7 +14,10 @@ class SongsController < ApplicationController
   end
 
   def create
-    @song = Song.create(params.require(:song).permit(:name, :artist_id, :genre_id))
+    @genre = Genre.find_or_create_by(name: params[:song][:genre_id])
+    @artist = Artist.find_or_create_by(name: params[:song][:artist_id])
+    @song = Song.create(name: params[:song][:name], artist_id: @artist.id, genre_id: @genre.id)
+    # @song = Song.create(params.require(:song).permit(:name, @artist.id, @genre.id)) ##This is for security. I don't know how to use it.
     redirect_to song_path(@song)
   end
 
